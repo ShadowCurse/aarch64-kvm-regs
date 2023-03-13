44a1e172f06477c062211aca6a83eeb2c2ac155d
ShadowCurse <shadowcurse57@gmail.com>
2023-03-13 16:28:11 +0000

    fix: init vcpu + updated register query logic

--- src/mimi_vm.rs
+++ src/mimi_vm.rs
@@ -11,6 +11,8 @@ pub enum Error {
     CreateVm(kvm_ioctls::Error),
     #[error("Can not create vcpu: {0}")]
     CreateVcpu(kvm_ioctls::Error),
+    #[error("Can not init vcpu: {0}")]
+    InitVcpu(kvm_ioctls::Error),
     #[error("Can not get reg list: {0}")]
     GetRegList(kvm_ioctls::Error),
 }
@@ -35,15 +37,27 @@ impl KvmVcpuWrapper {
         let vm = kvm.create_vm().map_err(Error::CreateVm)?;
         let vcpu = vm.create_vcpu(0).map_err(Error::CreateVcpu)?;
 
+        let mut kvi = kvm_bindings::kvm_vcpu_init::default();
+        vm.get_preferred_target(&mut kvi).unwrap();
+        vcpu.vcpu_init(&kvi).map_err(Error::InitVcpu)?;
+
         Ok(Self { kvm, vm, vcpu })
     }
 
     pub fn query_registers(&self) -> Result<Vec<KvmRegisterQuery>, Error> {
         let mut reg_list = RegList::new(Self::REGISTERS_TO_QUERY).unwrap();
-        self.vcpu
-            .get_reg_list(&mut reg_list)
-            .map_err(Error::GetRegList)?;
-        let reg_list = reg_list.as_slice();
+        let reg_list = match self.vcpu.get_reg_list(&mut reg_list) {
+            Ok(_) => reg_list.as_slice(),
+            Err(_) => {
+                // if we fail to get Self::REGISTERS_TO_QUERY then the `n` in reg_list 
+                // will contain the correct number of registers to query
+                reg_list = RegList::new(reg_list.as_fam_struct_ref().n as usize).unwrap();
+                self.vcpu
+                    .get_reg_list(&mut reg_list)
+                    .map_err(Error::GetRegList)?;
+                reg_list.as_slice()
+            }
+        };
 
         assert!(Self::REGISTERS_TO_QUERY < reg_list.len());
 
